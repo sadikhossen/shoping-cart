6555695259785f906264c0180107bf06dc9ed809
sadikhossen <sadikhossen82@gmail.com>
2022-02-16 17:29:35 +0600

    saddik

--- app.js
+++
@@ -1,8 +0,0 @@
-
-document.getElementById('case-plus').addEventListener('click', function () {
-    const caseInput = document.getElementById('case-count');
-    const caseNumber = caseInput.value;
-    caseInput.value = parseInt(caseNumber) + 1;
-
-})
-
